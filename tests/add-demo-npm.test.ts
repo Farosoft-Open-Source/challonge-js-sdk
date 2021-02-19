73542d14d25521e26e1955fe0078d094d4864674
Bryan Riley <brileydeveloper@gmail.com>
2021-02-19 16:28:56 -0500

    Adding initial setup commit

---
+++ tests/add-demo-npm.test.ts
@@ -0,0 +1,17 @@
+import { add } from "../src/add-demo-npm";
+import { expect } from "chai";
+
+describe("add-demo-npm unit tests", (): void => {
+    it("adding two positive numbers", (): void => {
+        const actual: number = add(1,2);
+        expect(actual).is.equal(3);
+    });
+    it("adding two negative numbers", (): void => {
+        const actual: number = add(-5,-2);
+        expect(actual).is.equal(-7);
+    });
+    it("adding one positive and one negative number", (): void => {
+        const actual: number = add(7,-2);
+        expect(actual).is.equal(5);
+    });
+});
